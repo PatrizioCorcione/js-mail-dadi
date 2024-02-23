6fcb2973faf5a514cf148053f43e776fc09e2a10
PatrizioCorcione <patriziocorcione1995@gmail.com>
2024-02-23 16:41:03 +0100

    [end] modifiche estetiche

--- js/script.js
+++ js/script.js
@@ -1,4 +1,6 @@
-/*sezione email*/
+
+/* SEZIONE E-MAIL*/
+
 const accessiConsentiti = ["franco.verde@gmail.com","ciccio.rosso@gmail.com","matte.giallo@gmail.com","steo.nero@gmail.com","ugo.deughi@gmail.com"]
 
 const btnJv = document.getElementById("btn")
@@ -6,14 +8,14 @@ const btnJv = document.getElementById("btn")
 const emailJs = document.getElementById("email");
 
 btnJv.addEventListener('click',function(event){
+  
   event.preventDefault();
-
 
   for (let i = 0; i < accessiConsentiti.length; i++) {
   if (emailJs.value == accessiConsentiti[i]) {
     document.getElementById("result").innerHTML = "Accesso acconsentito";
     i = accessiConsentiti.length;
-    /*ci sarebbe anche retur*/
+    /*CI SAREBBE ANCHE IL RETUR*/
 
     } else {
       document.getElementById("result").innerHTML = "Accesso negato";
@@ -22,35 +24,37 @@ btnJv.addEventListener('click',function(event){
   }
 })
 
-/*sezione dadi*/
-
-
+/*SEZIONE DADI*/
 
 btnDice.addEventListener('click',function(){
 
   numeroGio = Math.ceil(Math.random() * 6);
   numeroPc = Math.ceil(Math.random() * 6);
-  
+
+  document.getElementById("resultDice-pc").classList.add("dice-style");
+
+  document.getElementById("resultDice-gio").classList.add("dice-style");
+
   if (numeroGio > numeroPc) {
 
     document.getElementById("resultDice").innerHTML = "Hai vinto!";
     document.getElementById("resultDice-gio").innerHTML = numeroGio;
     document.getElementById("resultDice-pc").innerHTML = numeroPc;
+    
 
     }else if(numeroGio == numeroPc){
 
     document.getElementById("resultDice").innerHTML = "Pareggio!";
     document.getElementById("resultDice-gio").innerHTML = numeroGio;
     document.getElementById("resultDice-pc").innerHTML = numeroPc;
+    ;
 
     }else{
 
     document.getElementById("resultDice").innerHTML = "Hai perso!";
     document.getElementById("resultDice-gio").innerHTML = numeroGio;
     document.getElementById("resultDice-pc").innerHTML = numeroPc;
-
+    
   }
 
 })
-
-
